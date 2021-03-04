aa0d1f14d90742abf731875741c678a316f0f34c
gtarcea <glenn.tarcea@gmail.com>
2021-03-04 13:52:28 -0500

    Add additional fields such as ApiToken to User model

--- mcmodel/user.go
+++ mcmodel/user.go
@@ -1,7 +1,14 @@
 package mcmodel
 
+import "time"
+
 type User struct {
 	ID         int
+	UUID       string
+	Name       string
 	Email      string
 	GlobusUser string
+	ApiToken   string
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
 }
