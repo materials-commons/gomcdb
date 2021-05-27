32308ae427c3a929965246175472bc729b321348
gtarcea <glenn.tarcea@gmail.com>
2021-05-27 14:37:24 -0400

    Add EntityState definition and reference in Entity

--- mcmodel/entity.go
+++ mcmodel/entity.go
@@ -1,7 +1,13 @@
 package mcmodel
 
 type Entity struct {
-	ID    int
-	Name  string
-	Files []File `gorm:"many2many:entity2file"`
+	ID           int
+	Name         string
+	Files        []File `gorm:"many2many:entity2file"`
+	EntityStates []EntityState
+}
+
+type EntityState struct {
+	ID       int
+	EntityID int
 }
